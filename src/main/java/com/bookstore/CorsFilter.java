cbcd1d828ba49a31da5bee07c75347454d8c45de
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-05-14 06:54:54 +0530

    fixed 2

--- src/main/java/com/bookstore/CorsFilter.java
+++ src/main/java/com/bookstore/CorsFilter.java
@@ -3,23 +3,30 @@ package com.bookstore;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
+    private static final Logger LOGGER = Logger.getLogger(CorsFilter.class.getName());
+
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+        LOGGER.info("Applying CORS filter for request: " + requestContext.getMethod() + " " + requestContext.getUriInfo().getPath());
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        headers.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Max-Age", "1209600");
 
-        // Handle OPTIONS preflight requests
         if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
+            LOGGER.info("Handling OPTIONS preflight request");
             responseContext.setStatus(200);
         }
+        LOGGER.info("CORS headers added: " + headers.toString());
     }
 }
