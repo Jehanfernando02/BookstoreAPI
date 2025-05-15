d619433bbd83bc3755895bf6e0a6841679619b36
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-05-15 07:40:04 +0530

    fixed 7

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,23 +1,17 @@
 package com.bookstore;
 
-import com.bookstore.resource.*;
+import org.glassfish.jersey.server.ResourceConfig;
+
 import com.bookstore.exception.GenericExceptionMapper;
+
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("/api")
-public class BookstoreApplication extends Application {
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-        classes.add(BookResource.class);
-        classes.add(AuthorResource.class);
-        classes.add(CustomerResource.class);
-        classes.add(CartResource.class);
-        classes.add(GenericExceptionMapper.class);
-        classes.add(CorsFilter.class);
-        return classes;
+public class BookstoreApplication extends ResourceConfig {
+    public BookstoreApplication() {
+        packages("com.bookstore.resource");
+        register(CorsFilter.class);
+        register(GenericExceptionMapper.class);
+        register(org.glassfish.jersey.jackson.JacksonFeature.class);
     }
 }
