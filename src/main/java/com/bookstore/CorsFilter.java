4f17552bb7660ad0dfbc4a8be8fa7af30a079a7b
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-05-15 20:59:59 +0530

    fixed 11

--- src/main/java/com/bookstore/CorsFilter.java
+++ src/main/java/com/bookstore/CorsFilter.java
@@ -3,6 +3,7 @@ package com.bookstore;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.Arrays;
@@ -18,14 +19,24 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
         String origin = requestContext.getHeaderString("Origin");
+
+        // Set CORS headers for allowed origins
         if (origin != null && ALLOWED_ORIGINS.contains(origin)) {
-            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+            headers.add("Access-Control-Allow-Origin", origin);
         } else {
-            responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://libro-nest.vercel.app");
+            headers.add("Access-Control-Allow-Origin", "https://libro-nest.vercel.app");
         }
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        headers.add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Max-Age", "86400");
+
+        // Handle preflight OPTIONS requests
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            responseContext.setStatus(200); // OK for preflight
+        }
     }
 }
