bac878e1b440e68433d1fbed72d9c6da870f8f1c
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-05-15 21:32:50 +0530

    fixed some bugs

--- src/main/java/com/bookstore/resource/RootResource.java
+++ src/main/java/com/bookstore/resource/RootResource.java
@@ -1,14 +1,7 @@
-
 package com.bookstore.resource;
 
-/**
- *
- * This class defines the root endpoint of the Bookstore API.
- * When a user accesses the base path (/api/), it returns a welcome message in JSON format.
- */
-
-
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -19,12 +12,17 @@ import java.util.Map;
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 public class RootResource {
+
     @GET
     public Response getRoot() {
-        
-        // Return a welcome message for the API root endpoint
         Map<String, String> response = new HashMap<>();
         response.put("message", "Hello , Welcome to the Bookstore API.");
         return Response.ok(response).build();
+    }
+
+    @OPTIONS
+    public Response handlePreflight() {
+        // Respond with 200 OK and let CorsFilter add headers
+        return Response.ok().build();
     }
 }
